5163d43c06d8596d69c894f6f3d3ba90d2fd93d2
lniwn <lniwn@live.com>
2018-05-18 19:08:52 +0800

    [u] 完善插件注册接口

--- proxy_sites/__init__.py
+++ proxy_sites/__init__.py
@@ -16,15 +16,21 @@ class Registerable(abc.ABC):
         yield None
 
 
-def register_all():
-    # option 1
+def register_all(condition=None):
+    """
+    注册proxy_sites文件夹下所有插件，插件增删之后，需要重新调用此函数
+    外部可通过condition回调进行过滤，原型为bool condition(class name, class)
+    :param condition: 过滤条件，callable对象，返回True/False来控制是否注册对应插件
+    :return: None
+    """
+    # option 1 静态加载
     # step1: 指定__all__
     # step2: 导入所有模块from . import *
     # step3: 使用python3独有的方法__subclasses__获取所有子类
     # for sub in Registerable.__subclasses__():
     #     sub()
 
-    # option 2
+    # option 2 动态加载
     # step1: 获取所有模块名
     # step2: 使用importlib.import_module显示导入所有模块
     # step3: 循环遍历所有class
@@ -35,8 +41,10 @@ def register_all():
 
     files = glob.glob(os.path.join(os.path.dirname(__file__), '*.py'))
     modules = [os.path.basename(f)[:-3] for f in files if os.path.isfile(f) and not f.endswith('__init__.py')]
+    sites.clear()
     for n in modules:
         m = importlib.import_module(__name__ + '.' + n)
-        for _, obj in inspect.getmembers(m, inspect.isclass):
-            if issubclass(obj, Registerable) and (obj is not Registerable):
-                obj()
+        for name, obj in inspect.getmembers(m, inspect.isclass):
+            if callable(condition) and condition(name, obj):
+                if issubclass(obj, Registerable) and (obj is not Registerable):
+                    obj()
