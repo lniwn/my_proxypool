9e904e669259e78a5bf6206b14cffa062ae0a913
lniwn <lniwn@live.com>
2018-10-26 18:53:29 +0800

    [m] 增加ip.360.cn用于检测ip是否可用

--- datautil/webutils.py
+++ datautil/webutils.py
@@ -52,32 +52,72 @@ class ProxyValidator:
 
     async def is_useable(self, pp: models.ProxyTbl):
         try:
-            if pp.scheme.lower() == 'https':
-                url = 'http://ip.taobao.com/service/getIpInfo2.php?ip=myip'
-            else:
-                url = 'http://ip.taobao.com/service/getIpInfo2.php?ip=myip'
-            async with self._sess.get(url, proxy='{0}://{1}:{2}'.format(
-                                         pp.scheme if pp.scheme is not None else 'http',
-                                         pp.host,
-                                         pp.port)) as resp:
-                if resp.status != 200:
-                    return False, pp
-                try:
-                    json_pp = await resp.json(encoding='utf-8', content_type=None)
-                except json.JSONDecodeError:
-                    return False, pp
-                if (json_pp is None) \
-                        or (json_pp['code'] != 0) \
-                        or (json_pp['data']['ip'] != pp.host):
-                    return False, pp
-                new_pp = models.ProxyTbl(host=pp.host, port=pp.port,
-                                         scheme=pp.scheme if pp.scheme is not None else 'http',
-                                         country=json_pp['data']['country'],
-                                         area='%s.%s' % (json_pp['data']['region'], json_pp['data']['city']))
-                return True, new_pp
-
+            return await self._ip_check_360(pp)
         except (asyncio.TimeoutError, aiohttp.ClientError):
             return False, pp
+
+    async def _ip_check_taobao(self, pp: models.ProxyTbl) -> (bool, models.ProxyTbl):
+        if pp.scheme.lower() == 'https':
+            url = 'http://ip.taobao.com/service/getIpInfo2.php?ip=myip'
+        else:
+            url = 'http://ip.taobao.com/service/getIpInfo2.php?ip=myip'
+        async with self._sess.get(url, proxy='{0}://{1}:{2}'.format(
+                                     pp.scheme if pp.scheme is not None else 'http',
+                                     pp.host,
+                                     pp.port)) as resp:
+            if resp.status != 200:
+                return False, pp
+            try:
+                json_pp = await resp.json(encoding='utf-8', content_type=None)
+            except json.JSONDecodeError:
+                return False, pp
+            if (json_pp is None) \
+                    or (json_pp['code'] != 0) \
+                    or (json_pp['data']['ip'] != pp.host):
+                return False, pp
+            new_pp = models.ProxyTbl(host=pp.host, port=pp.port,
+                                     scheme=pp.scheme if pp.scheme is not None else 'http',
+                                     country=json_pp['data']['country'],
+                                     area='%s.%s' % (json_pp['data']['region'], json_pp['data']['city']))
+            return True, new_pp
+
+    async def _ip_check_360(self, pp: models.ProxyTbl) -> (bool, models.ProxyTbl):
+        async with self._sess.get('http://ip.360.cn/IPQuery/ipquery?ip=%s' % pp.host,
+                                  proxy='{0}://{1}:{2}'.format(
+                pp.scheme if pp.scheme is not None else 'http',
+                pp.host,
+                pp.port)) as resp:
+            if resp.status != 200:
+                return False, pp
+            try:
+                json_pp = await resp.json(encoding='utf-8', content_type=None)
+            except json.JSONDecodeError:
+                return False, pp
+            if (json_pp is None) or (json_pp['errno'] != 0):
+                return False, pp
+
+            loc_pair = json_pp['data'].strip().split('\t')
+            if len(loc_pair) == 1:
+                country = loc_pair[0]
+                area = 'XX'
+            elif len(loc_pair) == 2:
+                if any(t in loc_pair[0] for t in ('台湾', '香港', '澳门')):
+                    country = loc_pair[0][0:2]
+                    area = loc_pair[0][2:]
+                    if not area:
+                        area = 'XX'
+                else:
+                    country = '中国'
+                    area = loc_pair[0]
+            else:
+                mylog.warning('%s 无法解析', json_pp)
+                return False, pp
+
+            new_pp = models.ProxyTbl(host=pp.host, port=pp.port,
+                                     scheme=pp.scheme if pp.scheme is not None else 'http',
+                                     country=country,
+                                     area=area)
+            return True, new_pp
 
 
 class WebSpider:
@@ -130,7 +170,7 @@ class WebSpider:
         try:
             async with self._sess.get(url, proxy=proxy, **kwargs) as resp:
                 return resp.status, await resp.text()
-        except aiohttp.ClientError as err:
+        except (aiohttp.ClientError, aiohttp.ServerTimeoutError) as err:
             mylog.exception(err)
             return None, None
 
@@ -141,6 +181,6 @@ class WebSpider:
         async with self._sess.post(url, proxy=proxy, **kwargs) as resp:
             try:
                 return resp.status, await resp.text()
-            except aiohttp.ClientError as err:
+            except (aiohttp.ClientError, aiohttp.ServerTimeoutError) as err:
                 mylog.exception(err)
                 return None, None
